a02a78c45a625971e3c37af14b16809265ebca25
Mateusz Cieplinski <mateusz@coldrun.pl>
2022-01-12 16:08:07 +0100

    fix(config): add npm scope to the path

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const VUETIFY_PRESET_NAME = `sysdyne`;
-const VUETIFY_PRESET_PATH = `~vue-cli-plugin-vuetify-preset-${VUETIFY_PRESET_NAME}/preset`;
+const VUETIFY_PRESET_PATH = `~@sysdyne/vue-cli-plugin-vuetify-preset-${VUETIFY_PRESET_NAME}/preset`;
 
 module.exports = { VUETIFY_PRESET_NAME, VUETIFY_PRESET_PATH };
